9125d397c9f29dc036d016d42050b1944484946f
okoc <okoc@uni-bremen.de>
2025-06-23 18:09:20 +0200

    project

---
+++ TypeCasting.py
@@ -0,0 +1,20 @@
+
+name = "Öykü"
+age = 22
+gpa = 3.2
+isStudent = True
+
+#prints the type of the given variable
+print(type(name)) #<class 'str'>
+print(type(age)) #<class 'int'>
+
+#converts the function to the data type it wants to receive
+gpa = int(gpa)
+print(gpa) #3
+
+age = float(age)
+print(age) #22.0
+
+isStudent = int(isStudent)
+print(isStudent) #1
+
