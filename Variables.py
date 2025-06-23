f494a26bccb3da98d9b92639f8f2bd44e85ba4a6
okoc <okoc@uni-bremen.de>
2025-06-23 17:56:45 +0200

    project

---
+++ Variables.py
@@ -0,0 +1,58 @@
+#Strings
+firstName = "Öykü"
+#prints the variable
+print(firstName) # -> Öykü
+
+#variable
+secondName = "Yılmaz"
+#prints the variable according to given text with -> f"text {variable name}"
+print(f"Hello {secondName}")  # -> Hello Yılmaz
+
+email = "yilmaz123@fak.com"
+print(f"Your email is: {email}") # -> Your email is: yilmaz123@fak.com
+
+#Integers
+age = 22
+print(f"You're {age} years old!")
+
+quantity = 3
+print(f"You're buying {quantity} items!")
+
+numOfStudents = 30
+print(f"Your class has {numOfStudents} students!")
+
+#Float
+price = 10.99
+print(f"The price is ${price}")
+
+gpa = 3.2
+print(f"Your gpa is : {gpa}")
+
+distance = 5.5
+print(f"You ran {distance} km ! Great Job :)")
+
+#Boolean
+isStudent = False
+
+print(f"Are you a student? {isStudent}")
+if isStudent:
+    print("You're a student!")
+else:
+    print("You're not a student!")
+
+isForSale = True
+
+if isForSale:
+    print("That item is for sale!")
+else:
+    print("That item is not available")
+
+isOnline = False
+
+if isOnline:
+    print("You're online")
+else:
+    print("You're offline")
+
+
+
