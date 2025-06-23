e1bac6df222914c6e602b9f71c3dd6905d26c3fd
okoc <okoc@uni-bremen.de>
2025-06-23 18:20:44 +0200

    project

---
+++ UserInput.py
@@ -0,0 +1,15 @@
+#What is your name?: Öykü
+#Hello Öykü!
+name = input("What is your name?: ")
+print(f"Hello {name}!")
+
+#How old are you?: 23
+#You're 22 years old!
+age = input("How old are you?: ")
+
+age = int(age) #age = int(input("How old are you?: "))
+age = age + 1
+
+
+print(f"You're {age} years old!")
+
