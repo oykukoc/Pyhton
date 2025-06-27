ccff044c8c0d3de6bf3ea1c786086507a67d73a6
okoc <okoc@uni-bremen.de>
2025-06-27 21:08:07 +0200

    project

---
+++ MadlibsGame.py
@@ -0,0 +1,14 @@
+#Word game where you create a story
+#by filling in blanks with random words
+
+adjective1 = input(f"Enter an adjective : ")
+noun1 = input(f"Enter a noun: ")
+adjective2 = input(f"Enter an adjective : ")
+verb1 = input(f"Enter a verb ending with -ing : ")
+adjective3 = input(f"Enter an adjective : ")
+
+
+print(f"Today i went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}!")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
