9241ba172c9126024b920d3563c87b68c011f852
okoc <okoc@uni-bremen.de>
2025-06-27 22:50:25 +0200

    project

---
+++ IfStatement.py
@@ -0,0 +1,42 @@
+# #First Example
+from ftplib import print_line
+
+# age = int(input(f"Welcome to the Game! Please enter your age before you continue : "))
+#
+# print(age)
+# while age <= 0:
+#         print("The age can not be negative or zero!")
+#         age =int (input("Please enter your age again : "))
+# if age >= 18:
+#     print("Enjoy the game..")
+# else :
+#     print("This game contains strong language and violence.. You must be at least 18 to play this game!")
+
+# Second Exmaple
+
+print_line("List of drinks")
+print_line("1 - Vodka-Redbull")
+print_line("2 - Whiskey")
+print_line("3 - Beer")
+
+response = input("Would you like to drink some Alcohol? (Y/N) : ")
+
+while response != "Y" and response != "y" and response != "N" and response != "n" and response != " ":
+    print("Please give a valid answer!")
+    response = input("Would you like to drink some Alcohol? (Y/N) : ")
+if response == "Y" or  response ==  "y":
+    chooseDrinks = int(input("Please enter a the number according the drinks, that you would like to drink : "))
+    while chooseDrinks != 1 and chooseDrinks != 2 and chooseDrinks != 3:
+        print_line("List of drinks")
+        print_line("1 - Vodka-Redbull")
+        print_line("2 - Whiskey")
+        print_line("3 - Beer")
+        chooseDrinks = int(input("Please enter the number of alcohol according to the table, that you would like to drink : "))
+    if chooseDrinks == 1:
+            print("Enjoy your Vodka-Redbull!")
+    elif chooseDrinks == 2:
+            print("Enjoy your Whiskey!")
+    else:
+            print("Enjoy your Beer!")
+else:
+    print("Have a nice day!")
