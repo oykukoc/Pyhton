b5347edb3df5a6b15cdc3dd6805fa5d57197ab19
okoc <okoc@uni-bremen.de>
2025-06-27 21:01:11 +0200

    project

---
+++ UserInputExercise.py
@@ -0,0 +1,17 @@
+#Exercise 1 Rectangle Area Calc
+
+length = float (input("Enter the length: "))
+width = float(input("Enter the width: "))
+
+area = length * width
+print(f"The area is :  {area} cm")
+
+#Shopping Cart Program
+
+item = input("What item would you like to buy? : ")
+price = float(input("What is the price?: "))
+quantity = int (input("How Many would you like to have? : "))
+total = price * quantity
+
+print(f"You have to pay {total} $ for {quantity} {item}! ")
+
