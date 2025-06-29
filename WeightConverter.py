d79170116a4d621e18793fdf22e35b533ad23c1b
okoc <okoc@uni-bremen.de>
2025-06-29 15:02:10 +0200

    project

--- WeightConverter.py
+++ WeightConverter.py
@@ -1,2 +1,14 @@
 # Python weight converter :)
 
+weight = float(input("Enter your weight: "))
+choose = (input("Kilograms or Pounds? (K or L): "))
+
+if choose.upper() == "K":
+    result = weight * 2.205
+    print(f"Your weight is: {round(weight, 2)} Kgs.")
+elif choose.upper() == "L":
+    result = weight / 2.205
+    print(f"Your weight is: {round(weight, 2)} Lbs.")
+else :
+    print(f"{choose} is not valid!")
+
