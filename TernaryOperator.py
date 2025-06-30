a8c449e395b20a897bb9bf45b039198191856e8f
okoc <okoc@uni-bremen.de>
2025-06-30 18:43:57 +0200

    project

---
+++ TernaryOperator.py
@@ -0,0 +1,15 @@
+
+num = int(input("Please enter a number: "))
+
+print("Positive" if num > 0 else "Negative")
+result = "Even" if abs(num) % 2 == 0 else "Odd"
+print(result)
+
+num1 = int(input("Please enter a number: "))
+num2 = int(input("Please enter a number: "))
+maxNum = num1 if num1 > num2 else num2
+print(maxNum)
+
+age= int(input("Enter your age: "))
+staus = "Adult" if age >= 18 else "Child"
+print(staus)
